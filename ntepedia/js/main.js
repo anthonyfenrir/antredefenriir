3d05f9ad7d5ee94f60c5db4239fb1925ea76962c
FenriirCode <anthony.zedrac@gmail.com>
2026-05-10 02:38:31 +0200

    Create main.js

---
+++ ntepedia/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const backToTop = document.getElementById("backToTop");
+
+  if (!backToTop) return;
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+      backToTop.classList.add("show");
+    } else {
+      backToTop.classList.remove("show");
+    }
+  });
+
+  backToTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
